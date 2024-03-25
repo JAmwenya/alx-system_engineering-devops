a16e482816d4d357ffa252a3aa5a8b070371c9fb
JAmwenya <jnehondo@gmail.com>
2024-03-25 22:37:35 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,11 +6,8 @@ import json
 
 if __name__ == "__main__":
     response = requests.get('https://jsonplaceholder.typicode.com/todos')
-#print(response.status_code)
-#print(response.json())
 
     data = response.json()
-
     user_id = int(input("insert your ID number: "))
 
 # Filter tasks for the given user_id
@@ -18,14 +15,14 @@ if __name__ == "__main__":
 
 # Count total tasks and completed tasks
     total_tasks = len(user_tasks)
-    completed_tasks = sum(task['completed'] for task in user_tasks)
+    tasks_done = sum(task['completed'] for task in user_tasks)
 
 # Fetch user info
     user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
     employee_name = user_info['name']
 
 # Print progress
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} has done ({tasks_done}/{total_tasks}):")
 
 # Print titles of completed tasks
     for task in user_tasks:
