b236130098963a1d6a53e13a18293534bed77305
JAmwenya <jnehondo@gmail.com>
2024-03-26 11:27:19 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,29 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 '''python script that returns a ToDo list progress when given an ID'''
-import requests
 import json
-
+import requests
 
 if __name__ == "__main__":
-    response = requests.get('https://jsonplaceholder.typicode.com/todos')
+     response = requests.get('https://jsonplaceholder.typicode.com/todos')
+     data = response.json()
+     user_id = int(input("insert your ID number: "))
 
-    data = response.json()
-    user_id = int(input("insert your ID number: "))
+ # Filter tasks for the given user_id
+     user_tasks = [task for task in data if task['userId'] == user_id]
 
-# Filter tasks for the given user_id
-    user_tasks = [task for task in data if task['userId'] == user_id]
+ # Count total tasks and completed tasks
+     total_tasks = len(user_tasks)
+     tasks_done = sum(task['completed'] for task in user_tasks)
 
-# Count total tasks and completed tasks
-    total_tasks = len(user_tasks)
-    tasks_done = sum(task['completed'] for task in user_tasks)
+ # Fetch user info
+     user_info = requests.get
+     (f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
+     employee_name = user_info['name']
 
-# Fetch user info
-    user_info = requests.get
-    (f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
-    employee_name = user_info['name']
-
-# Print progress
-    print(f"Employee {employee_name} has done ({tasks_done}/{total_tasks}):")
-
-# Print titles of completed tasks
-    for task in user_tasks:
-        if task['completed']:
-            print(f"\t{task['title']}")
+ # Print progress
+     print(f"Employee {employee_name} has done ({tasks_done}/{total_tasks}):")
+ 
+  # Print titles of completed tasks
+     for task in user_tasks:
+         if task['completed']:
+             print(f"\t{task['title']}")
