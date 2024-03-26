078ac2631f68e3e30cb2282d2207586f60920d50
JAmwenya <jnehondo@gmail.com>
2024-03-26 19:55:04 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 '''python script that returns a ToDo list progress when given an ID'''
 import json
-import requests
-import sys
+from requests import get
+from sys import argv
 
 if __name__ == "__main__":
-    response = requests.get('https://jsonplaceholder.typicode.com/todos')
+    if len(argv) != 2:
+        print("Usage: python script.py <user_id>")
+        exit(1)
+
+    user_id = int(argv[1])  # Convert user ID to integer
+
+    # Fetching todos data
+    response = get('https://jsonplaceholder.typicode.com/todos')
     data = response.json()
-    user_id = int(input("insert your ID number: "))
 
+    # Filter tasks by user ID
     user_tasks = [task for task in data if task['userId'] == user_id]
-
     total_tasks = len(user_tasks)
     tasks_done = sum(task['completed'] for task in user_tasks)
 
-    user_info = requests.get
-    ('https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
+    # Fetching user info
+    response = get(
+'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    )
+    user_info = response.json()
     employee_name = user_info['name']
 
-    print("Employee {} has done ({}/{})".format(employee_name, tasks_done,
-    total_tasks))
-
+    # Printing task completion status
+    print(
+"Employee {} has done ({}/{}) tasks:".format(employee_name, tasks_done, total_tasks)
+        )
     for task in user_tasks:
         if task['completed']:
             print("\t{}".format(task['title']))
