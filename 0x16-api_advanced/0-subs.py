09d174168b7627a10033991514b70c6c9f3ff85b
JAmwenya <jnehondo@gmail.com>
2024-04-11 20:41:26 +0000

    a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,9 @@ def number_of_subscribers(subreddit):
 	"""
 	a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
 	"""
+
     url = f"https://www.reddit.com/r/dev/api/about.json"
-    headers = {'User-Agent': ''}  
+    headers = {'User-Agent': 'Google Chrome Version 123.0.6312.105/106'}  
     response = requests.get(url, headers=headers, allow_redirects=False)
     
     if response.status_code == 200:
